af8efa09cfb82ab9cf9da6d112a50c003858b72b
DESKTOP-T51FB32\Mefistofel <Mfstfl@gmail.com>
2018-10-15 22:39:38 +0300

    Added polygon stroke

--- html/object3d.js
+++ html/object3d.js
@@ -54,6 +54,7 @@ class Object3DTriangle {
 		if (dot > 0)
 			return
 		canvas.fillStyle = this.color;
+		canvas.strokeStyle = this.color;
 		canvas.beginPath();
 		let last = this.screenPoints[this.screenPoints.length - 1]
 		canvas.moveTo(last[0], last[1]);
@@ -62,43 +63,7 @@ class Object3DTriangle {
 		}
 		canvas.closePath();
 		canvas.fill();
-	}
-}
-// 3d Quad class
-class Object3DQuad {
-    constructor(a, b, c, d, color = '#FFEEEE') {
-		function findMiddlePoint (points) {
-			let sum = CreateVector3()
-			for(let i = 0; i < points.length; i++) {
-				sum = AddVector3(sum, points[i])
-			}
-			return MultiplyVector3(sum, 1.0 / parseFloat(points.length))
-		}
-
-		this.points = [a, b, c, d]
-		this.screenPoints = [a, b, c, d]
-		this.position = findMiddlePoint(this.points)
-		this.screenPosition = CreateVector3()
-		this.color = color
-	}
-	
-	prepareScene (camera) {
-		this.screenPosition = camera.worldToScreenVector3(this.position)
-		for(let i = 0; i < this.screenPoints.length; i++) {
-			this.screenPoints[i] = camera.worldToScreenVector3(this.points[i])
-		}
-	}
-
-	draw (canvas) {
-		canvas.fillStyle = this.color;
-		canvas.beginPath();
-		let last = this.screenPoints[this.screenPoints.length - 1]
-		canvas.moveTo(last[0], last[1]);
-		for(let i = 0; i < this.screenPoints.length; i++) {
-			canvas.lineTo(this.screenPoints[i][0], this.screenPoints[i][1]);
-		}
-		canvas.closePath();
-		canvas.fill();
+		canvas.stroke();
 	}
 }
 
