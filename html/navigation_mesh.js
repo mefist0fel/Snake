7a719744a151faa656df7ceacac603928a521a51
k.kostromin <k.kostomin@corpwebgames.com>
2018-10-17 20:54:44 +0300

    Move logic to nav mesh

--- html/navigation_mesh.js
+++ html/navigation_mesh.js
@@ -1,8 +1,21 @@
 class NavigationMesh {
-    constructor() {
-        this.triangles = []
+    constructor(navigationNodes = [], startNodeId = 0) {
+        this.nodes = navigationNodes
+        this.currentNode = this.nodes[startNodeId]
+        this.heroPosition = this.currentNode.center
+        this.heroVector = this.currentNode.unitTangent
+        // find neigbhors
     }
+
     push (navigationTriangle) {
         this.triangles.push(navigationTriangle)
+    }
+
+    moveHero(distance = 1.0) {
+        this.heroPosition = AddVector3(this.heroPosition, MultiplyVector3(this.heroVector, distance))
+    }
+
+    rotateHeroDirection(angle = 0.0) {
+        this.heroVector = MultiplyVector3ToMatrix3(this.heroVector, CreateRotationMatrix3(this.currentNode.unitNormal, angle))
     }
 }
